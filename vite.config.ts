de7430051fcdecd4ebc4bc24268dd71425b6bf69
axunion <120321953+axunion@users.noreply.github.com>
2023-08-29 20:02:22 +0900

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  resolve: {
+    alias: [{ find: '@', replacement: resolve(__dirname, 'src') }],
+  },
+  build: {
+    target: 'es2015',
+    emptyOutDir: true,
+  },
+});
