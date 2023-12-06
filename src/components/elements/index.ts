f6540296693cc1dcced28c01b53709f03b566e7a
axunion <120321953+axunion@users.noreply.github.com>
2023-12-06 16:20:21 +0900

    Correct missing export in component

--- src/components/elements/index.ts
+++ src/components/elements/index.ts
@@ -1,4 +1,5 @@
 export { ButtonComponent } from "./button/Button";
+export { CanvasComponent } from "./canvas/Canvas";
 export { InputFileComponent } from "./input-file/InputFile";
 export { InputTextComponent } from "./input-text/InputText";
 export { SelectComponent } from "./select/Select";
